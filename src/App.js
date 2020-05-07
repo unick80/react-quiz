4ab3417efe7cc4ce277563e8084e3c3850c920f6
Nick Pisarev <zagis-net@mail.ru>
2020-05-07 16:26:36 +0300

    043 Создание Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Layout>
+        <div style={{width: 400, border: '1px solid black'}}>
+          <h1>Layout works</h1>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default App;
