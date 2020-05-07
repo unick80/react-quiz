ab6e472ff6f1f499b09a095a89c831e0e457e8bd
Nick <zagis-net@mail.ru>
2020-05-07 23:50:54 +0300

    046 Создание главной страницы

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Quiz.css';
+
+class Quiz extends Component {
+    state = {
+        quiz: []
+    }
+    render(){
+        return(
+            <div className={'Quiz'}>
+                <h1>Quiz</h1>
+            </div>
+        )
+    }
+}
+
+export default Quiz
